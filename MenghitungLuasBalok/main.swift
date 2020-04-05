a4fc4c740c64fbd06c0db14d560cedc9de382d3e
Senno Hananto <sennohananto@gmail.com>
2020-04-06 00:28:57 +0700

    Initial Commit

---
+++ MenghitungLuasBalok/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MenghitungLuasBalok
+//
+//  Created by Senno Hananto on 06/04/20.
+//  Copyright © 2020 Senno Hananto. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
