fbcd3782debb8be56f7a123754cea0ca1463a50e
Senno Hananto <sennohananto@gmail.com>
2020-04-06 00:47:59 +0700

    init commit

--- MenghitungLuasBalok/main.swift
+++ MenghitungLuasBalok/main.swift
@@ -8,5 +8,31 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Panjang balok : "); let lengthInput = readLine();
+print("Lebar balok : "); let widthInput = readLine();
+print("Tinggi balok : "); let heightInput = readLine();
+
+if let length = Double(lengthInput ?? "0"), let height = Double(heightInput ?? "0"), let width = Double(widthInput ?? "0"){
+    // Ini adalah proses penghitungan yang dilakukan dari program yang Anda buat
+    let volume = length * height * width
+    let surfaceArea = 2 * ((width * length) + (width * height) + (height * length))
+    let circumference = 4 * (width + length + height)
+    
+    print("----------------------------------")
+    // Ini adalah output dari program yang Anda buat
+    print("Anda memiliki sebuah balok dengan:")
+    print("Lebarnya adalah \(width) cm")
+    print("Tingginya adalah \(height) cm")
+    print("Panjangnya adalah \(length) cm")
+    print("Volemenya adalah \(volume) cm3")
+    print("Luas permukaannya adalah \(surfaceArea) cm2")
+    print("Kelilingnya adalah \(circumference) cm")
+    print("----------------------------------")
+} else {
+    // Ketika input tidak valid
+    print("----------------------------------")
+    print("Input tidak valid")
+    print("----------------------------------")
+}
+
 
